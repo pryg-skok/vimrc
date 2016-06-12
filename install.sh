85aa53cf962f5183a78c0698f557a0cd544ce6d2
Vasiliy Volkov <volk.vasiliy@gmail.com>
2016-06-12 17:31:16 +0300

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
-!/bin/bash
+#!/bin/bash
 
-
+mkdir -p "$HOME/.vim"
+mkdir -p "$HOME/.vim/{autoload,tmp}"
+mkdir -p "$HOME/.vim/tmp/{undo,backup,swap}"
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
-mkdir -p "$HOME/.vim"
 ln -s $(pwd)/NERDTreeBookmarks $HOME/.vim/.NERDTreeBookmarks
 ln -s $(pwd)/functions.vim $HOME/.vim/autoload/functions.vim
 ln -s $(pwd)/templates $HOME/.vim/templates
 
-
 # vim-plug install
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#echo "OK! Now VIM will fail to start but open it and run :PlugInstall"
-echo "Success!"
+echo "OK! Now VIM will fail to start but open it and run :PlugInstall"
